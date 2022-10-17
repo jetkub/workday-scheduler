445bb7c489212dc950df70f89d5c939ac480e887
jakub <jakub@uconn.edu>
2022-10-16 20:12:36 -0400

    start loadTasks function, move whitespace trim() for save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,21 @@ currentDayEl.text(currentDate);
 // add on-click listener to saveBtn; save input in textarea
 // and the listed time in the col-2 hour div to local storage (setItem)
 $(".saveBtn").on("click", function(){
-  let taskTimeRaw = $(this).siblings(".hour").text();
-  // trim whitespace
-  let taskTime = taskTimeRaw.trim();
+  let taskTime = $(this).siblings(".hour").text().trim(); // trim whitespace
   let taskInput = $(this).siblings("textarea").val();
-  console.log("time: " + taskTime)
-  console.log("input: " + taskInput)
+
+  console.log("time: " + taskTime);
+  console.log("input: " + taskInput);
 
   localStorage.setItem(taskTime, taskInput);
 });
 
 
 // funcion to load saved tasks from localStorage (getItem)
+function loadTasks(){
+  $(".hour").each(function(){
+    let taskTime = $(this).text().trim();
+    let taskData = localStorage.getItem(taskTime);
+    
+  });
+}
