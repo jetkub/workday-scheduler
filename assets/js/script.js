2e652df61466e1ca4b91c18965f57ab8c6db2660
jakub <jakub@uconn.edu>
2022-10-16 20:04:57 -0400

    add save button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 // current day
 var currentDate = moment().format("dddd, MMMM Do");
 // set date in jumbotron
-var currentDayEl = $("#currentDay")
+var currentDayEl = $("#currentDay");
 currentDayEl.text(currentDate);
+
+// save button functionality
+// add on-click listener to saveBtn; save input in textarea
+// and the listed time in the col-2 hour div to local storage (setItem)
+$(".saveBtn").on("click", function(){
+  let taskTimeRaw = $(this).siblings(".hour").text();
+  // trim whitespace
+  let taskTime = taskTimeRaw.trim();
+  let taskInput = $(this).siblings("textarea").val();
+  console.log("time: " + taskTime)
+  console.log("input: " + taskInput)
+
+  localStorage.setItem(taskTime, taskInput);
+});
+
+
+// funcion to load saved tasks from localStorage (getItem)
